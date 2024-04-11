7b4690d56aad68305fb01d858654e3965335bdfb
Yuri Gomes <yurialdegomes@hotmail.com>
2024-04-11 02:58:14 -0300

    residencias

--- app/routes/v1/residencias/routes.py
+++ app/routes/v1/residencias/routes.py
@@ -6,18 +6,21 @@ from app.routes.auth.routes import token_required
 residencias_bp = Blueprint('residencias', __name__, url_prefix='/residencias')
 
 @residencias_bp.route('', methods=['GET'])
-#@token_required
-def get():
+@token_required
+def get(current_user):
 
     pi_usuario = request.args.get('usuario')
 
     usuario = crud_usuario.read(id_publico=pi_usuario)
 
+    if not usuario:
+        usuario = current_user
+
     try:
         residencias = crud_residencia.read_multi(id_usuario=usuario.id, schema=True)
-        return jsonify([{"result": residencias, "code": 200}])
+        return jsonify({"result": residencias, "code": 200})
     except:
-        return jsonify([{"message": "ID do usuario incorreto", "code": 404}])
+        return jsonify({"message": "ID do usuario incorreto", "code": 404})
 
 @residencias_bp.route('<int:residencia_id>/areas', methods=['GET'])
 #@token_required
